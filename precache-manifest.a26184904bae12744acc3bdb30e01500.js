95dc17246346443298e66c5a9e4fcbe072e2bbb1
Rhanon Macley <rh.macley@gmail.com>
2022-11-29 18:52:34 -0300

    Updates

---
+++ precache-manifest.a26184904bae12744acc3bdb30e01500.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff03bc42ae6ba980d8323a505003d7e",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "8a7afcd1a8a4ebfddf3d",
+    "url": "/react-portfolio/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "d6285c27f1bc93c44e02",
+    "url": "/react-portfolio/static/css/main.0628440c.chunk.css"
+  },
+  {
+    "revision": "8a7afcd1a8a4ebfddf3d",
+    "url": "/react-portfolio/static/js/2.0e2dd0a3.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/react-portfolio/static/js/2.0e2dd0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6285c27f1bc93c44e02",
+    "url": "/react-portfolio/static/js/main.d39b938c.chunk.js"
+  },
+  {
+    "revision": "b9f89b4b7cdb042de4e4",
+    "url": "/react-portfolio/static/js/runtime-main.e24be1f9.js"
+  }
+]);
